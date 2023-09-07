70b61cdc9b7e1d3337d59a36ffff988ec591982b
Sahil sen <sensahil25@gmail.com>
2023-09-07 23:20:25 +0530

    Create deleteNotification.js

---
+++ deleteNotification.js
@@ -0,0 +1,23 @@
+const API_KEY = 'QuickALerts_API_Key_Here'; // QuickAlerts API Key
+
+async function deleteNotification(notificationResponse) {
+
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append('accept', 'application/json');
+        myHeaders.append('Content-Type', 'application/json');
+        myHeaders.append('x-api-key', API_KEY);
+
+        const requestOptions = {
+            method: 'DELETE',
+            headers: myHeaders,
+            redirect: 'follow',
+        }
+        const response = await fetch(`https://api.quicknode.com/quickalerts/rest/v1/notifications/${encodeURIComponent(notificationResponse)}`, requestOptions);
+        return response
+    } catch(error) {
+            console.log('error', error);
+    }
+}
+
+export default deleteNotification;
